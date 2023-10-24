c6896d453834275558acb801ae676c8badce44b0
kevin arnold flores pacheco <arnold_k2001@hotmail.com>
2023-10-24 08:00:24 -0500

    addes button close

--- db/env.js
+++ db/env.js
@@ -1,5 +1,5 @@
-// const BASE_URL = 'https://apiubiponi.onrender.com/api/'
-const BASE_URL = 'http://localhost:3000/api/'
+const BASE_URL = 'https://apiubiponi.onrender.com/api/'
+// const BASE_URL = 'http://localhost:3000/api/'
 // const BASE_URL = 'http://127.0.0.1:8000/api/'
 
 const MAP_DARK = 'https://api.mapbox.com/styles/v1/mantequillita21/clnqi8rln00a501qjexvxd4bx/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoibWFudGVxdWlsbGl0YTIxIiwiYSI6ImNrbXh4MGZwOTAzMGwydnBnNXV4NG91bjMifQ.wljneJmPr3jJUSgzLB6YTg' 
